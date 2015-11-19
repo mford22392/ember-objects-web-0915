b612978980e6ae6a8d1da777e7c4a4fa0805b6f8
mford22392 <mford22392@gmail.com>
2015-11-19 17:17:53 -0500

    all but validation

--- app/lib/programmer.js
+++ app/lib/programmer.js
@@ -1,4 +1,57 @@
 import Ember from 'ember';
 
-export default Ember.Object.extend({
+var Programmer = Ember.Object.extend({
+  greet: function(){
+    return ("Hi, My name is " + this.firstName + " " + this.lastName + ". You can call me " + this.nickName);
+  },
+
+  isOld: Ember.computed.gte('age', 30),
+
+  wroteRuby: Ember.computed.equal('authorOf', "Ruby"),
+
+  addConference: function(conference){
+    return this.conferences.push(conference);
+  },
+
+  keyNoteConferences: Ember.computed('conferences.@each.keyNote', function() {
+    var name = this.firstName + " " + this.lastName;
+    var conferences = this.get('conferences');
+    var myConf = conferences.filterBy('keyNote', name);
+    // var names =  myConf.map(function(conf) {
+    //       return conf.name;
+    //     });
+  return myConf;
+  }),
+
+  conferenceNames: Ember.computed('conferences.@each.keyNote', function() {
+    var names =  this.conferences.map(function(conf) {
+          return conf.name;
+        });
+    return names;
+  }),
+
+  // conferenceTotal: this.get("keyNoteConferences").length,
+
+  conferenceTotal: Ember.computed("conferences", function(){
+    return this.get("conferences").length;
+  }),
+
+  itinerary: Ember.computed("conferenceTotal", function(){
+    return (this.nickName + " is speaking at " + this.get("conferenceTotal") + " conferences");
+  }),
+
+  hasValidEmail: Ember.computed.match('email', /^.+@.+\..+$/)
+
+  // isInvalid: (),
+
+  // isValid: (),
+
+  // errors: (),
+
+  // hasErrors: ()
+
+
 });
+
+
+export default Programmer;
